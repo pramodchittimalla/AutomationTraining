a71722ed94c097c449c2cdebf2b3359ac5694867
pramod chittimilla <pramodchittimalla@gmail.com>
2026-07-01 00:34:33 +0530

    Initial commit

--- assignments/assignment3.ts
+++ assignments/assignment3.ts
@@ -1,10 +1,14 @@
 let sname:string[] = ["suresh", "mahesh" , "naresh"];
 
 let smarks:number[]=[75,80,82];
-let sum:number
-console.log(smarks.length);
+let updatedMarks:number;
+let avg:number=0;
 
-for(const updatedMarks of smarks){
-    ;
+
+for(const value of smarks){
+    updatedMarks=10+value;
+    console.log(updatedMarks);
+    avg = avg+updatedMarks;
 }
-console.log(smarks[]);
+
+console.log(avg/3);
